46879e99782e08e4525b866bcb2796c83456770b
Patrick Gerritsen <p.gerritsen@wearetriple.com>
2023-05-17 15:53:51 +0200

    feat: change characters

--- src/characters.ts
+++ src/characters.ts
@@ -1,15 +1,21 @@
-import redPowerRangerImage from './assets/red-power-ranger.jpg';
-import bluePowerRangerImage from './assets/blue-power-ranger.jpg';
-import yellowPowerRangerImage from './assets/yellow-power-ranger.jpg';
-import pinkPowerRangerImage from './assets/pink-power-ranger.jpg';
-import blackPowerRangerImage from './assets/black-power-ranger.jpg';
+import image1 from './assets/duo_1.png';
+import image2 from './assets/duo_2.png';
+import image3 from './assets/duo_3.png';
+import image4 from './assets/duo_4.png';
+import image5 from './assets/duo_5.png';
 
-const characters = [
-  { id: '1', name: 'Red Power Ranger', image: redPowerRangerImage },
-  { id: '2', name: 'Blue Power Ranger', image: bluePowerRangerImage },
-  { id: '3', name: 'Yellow Power Ranger', image: yellowPowerRangerImage },
-  { id: '4', name: 'Pink Power Ranger', image: pinkPowerRangerImage },
-  { id: '5', name: 'Black Power Ranger', image: blackPowerRangerImage },
+type Character = {
+  id: string;
+  name: string;
+  image: string;
+};
+
+const characters: Character[] = [
+  { id: '1', name: 'Rhythm & Rhyme', image: image1 },
+  { id: '2', name: 'The Groove Squad', image: image2 },
+  { id: '3', name: 'Sound Waves', image: image3 },
+  { id: '4', name: 'Syncopation Station', image: image4 },
+  { id: '5', name: 'The Mix Master', image: image5 },
 ];
 
 export default characters;
